434841aafe22c1e7f92316ad43c6ca55ee4da4af
gurutracks <116145396+gurutracks@users.noreply.github.com>
2022-10-21 12:16:56 +0500

    Update update.php

--- update.php
+++ update.php
@@ -21,21 +21,21 @@ $row = mysqli_fetch_assoc($result);
     <title>Update Record</title>
 </head>
 <body>
+    <h1>CRUD application with PHP and MySQL</h1>
     <!-- HTML form with old data. Form submits to itself to execute PHP code bellow -->
     <form action="<?php ($_SERVER["PHP_SELF"]); ?>" method="post">
         <label>Name</label><br>
-        <input type="text" name="form_name" value="<?php echo $row["name"]; ?> "><br>
+        <input type="text" name="form_name" value="<?php echo $row["name"]; ?> " size="50"><br>
         <label>Address</label><br>
-        <input type="text" name="form_address" value="<?php echo $row["address"]; ?> "><br>
+        <input type="text" name="form_address" value="<?php echo $row["address"]; ?> " size="50"><br>
         <label>Phone</label><br>
-        <input type="text" name="form_phone" value="<?php echo $row["phone"]; ?> "><br>
+        <input type="text" name="form_phone" value="<?php echo $row["phone"]; ?> " size="50"><br>
         <label>Email</label><br>
-        <input type="text" name="form_email" value="<?php echo $row["email"]; ?> "><br>
+        <input type="text" name="form_email" value="<?php echo $row["email"]; ?> " size="50"><br><br>
         <input type="submit">
     </form>
 </body>
 </html>
-
 <?php  
     // If form submits and method is POST? Get form data into variables
     if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -43,10 +43,8 @@ $row = mysqli_fetch_assoc($result);
         $input_address = $_POST["form_address"];
         $input_phone = $_POST["form_phone"];
         $input_email = $_POST["form_email"];
-
         // Update record with new changed data
         $sql = "UPDATE address_table SET name='$input_name', address='$input_address', phone='$input_phone', email='$input_email' where id='$id'";
-
         // Execute the query
         if(mysqli_query($conn, $sql)){
             // If successfull, redirect to landing page
